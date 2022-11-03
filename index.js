0eabfb122bce696f94133f4f0125d2465a62053b
tor <torfrancis447@gmail.com>
2022-11-03 17:04:33 -0400

    commit tree f0b7f5889f66d586b8051f7d301bb74115075f88

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+        return array.slice(0,2);
+    }
+const returnLastTwoDrivers = (fullDriver) => {
+        return fullDriver.slice(-2)
+    }
+
+const selectingDrivers = [ returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (x) => {
+    return function(multiplier) {
+        return x * multiplier
+    }
+}
+const fareDoubler = createFareMultiplier(2) 
+const fareTripler = createFareMultiplier(3) 
+    
